981de03f32744fdfe4556073f1a37bd846c7d5c4
noboru-i <ishikura.noboru@gmail.com>
2017-01-05 23:36:26 +0900

    use datastore

--- Gemfile
+++ Gemfile
@@ -16,6 +16,9 @@ gem 'puma', '~> 3.0'
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
 
+gem 'googleauth', '~> 0.5.1'
+gem 'google-api-client', '~> 0.9.20'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,33 @@
+require 'googleauth'
+require 'google/apis/datastore_v1'
+
 class WelcomeController < ApplicationController
+  ServiceAccountCredentials  = Google::Auth::ServiceAccountCredentials
+  Datastore  = Google::Apis::DatastoreV1
+  RunQueryRequest = Google::Apis::DatastoreV1::RunQueryRequest
+  GqlQuery = Google::Apis::DatastoreV1::GqlQuery
+  GqlQueryParameter = Google::Apis::DatastoreV1::GqlQueryParameter
+  Value = Google::Apis::DatastoreV1::Value
+
   def index
-    kyouen = Struct.new("Dog", :name, :age)
-    kyouens = kyouen.new('welcome kyouen server', 98)
-    render json: kyouens
+    datastore = Google::Apis::DatastoreV1::DatastoreService.new
+
+    datastore.authorization = ServiceAccountCredentials.make_creds(
+        json_key_io: File.open('cert/my-android-server-91c8d931db89.json'),
+        scope: [
+          Datastore::AUTH_CLOUD_PLATFORM,
+          Datastore::AUTH_DATASTORE
+        ]
+    )
+    datastore.authorization.fetch_access_token!
+
+    query = GqlQuery.new(
+        query_string: 'SELECT * FROM GcmModel LIMIT 5',
+        allow_literals: true
+    )
+    request = RunQueryRequest.new(gql_query: query)
+    result = datastore.run_project_query('my-android-server', request)
+    print result.batch.entity_results[0].entity.as_json
+    render json: result.batch.entity_results[0].entity.as_json
   end
 end
