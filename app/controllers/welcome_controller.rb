d1a59fadd6d8bcea720f8d31fdfac3447adf3469
noboru-i <ishikura.noboru@gmail.com>
2017-01-11 00:01:16 +0900

    make module

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,43 +1,11 @@
-require 'googleauth'
-require 'google/apis/datastore_v1'
-
 class WelcomeController < ApplicationController
-  # https://github.com/google/google-api-ruby-client/blob/master/generated/google/apis/datastore_v1/classes.rb
-  ServiceAccountCredentials  = Google::Auth::ServiceAccountCredentials
-  Datastore  = Google::Apis::DatastoreV1
-  RunQueryRequest = Google::Apis::DatastoreV1::RunQueryRequest
-  GqlQuery = Google::Apis::DatastoreV1::GqlQuery
-  GqlQueryParameter = Google::Apis::DatastoreV1::GqlQueryParameter
-  Value = Google::Apis::DatastoreV1::Value
 
   def index
-    datastore = Google::Apis::DatastoreV1::DatastoreService.new
-
-    dummyFile = DummyFile.new(ENV['GCP_KEY']) if ENV['GCP_KEY'].present?
-    datastore.authorization = ServiceAccountCredentials.make_creds(
-        json_key_io: dummyFile || File.open('cert/my-android-server-91c8d931db89.json'),
-        scope: [
-          Datastore::AUTH_CLOUD_PLATFORM,
-          Datastore::AUTH_DATASTORE
-        ]
+    client = Datastore::Client.new
+    result = client.query(
+      'SELECT * FROM KyouenPuzzle ORDER BY stageNo LIMIT @1',
+      [Datastore::Parameter.new(5)]
     )
-
-    query = GqlQuery.new(
-        query_string: 'SELECT * FROM GcmModel LIMIT @limit',
-        named_bindings: {
-          limit: GqlQueryParameter.new(value: Value.new(integer_value: '5'))
-        }
-    )
-    request = RunQueryRequest.new(gql_query: query)
-    result = datastore.run_project_query('my-android-server', request)
-    render json: result.batch.entity_results.size
-    # render json: result.batch.entity_results[0].entity.as_json
-  end
-
-  class DummyFile
-    attr_accessor :read
-    def initialize(read)
-       @read = read
-    end
+    render json: result[0].entity.as_json
   end
 end
