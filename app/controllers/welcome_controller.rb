3f6c5a2a5a0cd2748eb9f173bf2dc0b067a739b8
noboru-i <ishikura.noboru@gmail.com>
2017-01-10 20:07:30 +0900

    delete unused lines

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ require 'googleauth'
 require 'google/apis/datastore_v1'
 
 class WelcomeController < ApplicationController
+  # https://github.com/google/google-api-ruby-client/blob/master/generated/google/apis/datastore_v1/classes.rb
   ServiceAccountCredentials  = Google::Auth::ServiceAccountCredentials
   Datastore  = Google::Apis::DatastoreV1
   RunQueryRequest = Google::Apis::DatastoreV1::RunQueryRequest
@@ -12,7 +13,6 @@ class WelcomeController < ApplicationController
   def index
     datastore = Google::Apis::DatastoreV1::DatastoreService.new
 
-    dummyFile = nil
     dummyFile = DummyFile.new(ENV['GCP_KEY']) if ENV['GCP_KEY'].present?
     datastore.authorization = ServiceAccountCredentials.make_creds(
         json_key_io: dummyFile || File.open('cert/my-android-server-91c8d931db89.json'),
@@ -30,9 +30,8 @@ class WelcomeController < ApplicationController
     )
     request = RunQueryRequest.new(gql_query: query)
     result = datastore.run_project_query('my-android-server', request)
-    print result.batch.entity_results[0].entity.as_json
-    # render json: result.batch.entity_results[0].entity.as_json
     render json: result.batch.entity_results.size
+    # render json: result.batch.entity_results[0].entity.as_json
   end
 
   class DummyFile
