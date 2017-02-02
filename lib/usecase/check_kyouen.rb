9e8ab3c31cdd988026acf964fd5a790e24aeb04d
noboru-i <ishikura.noboru@gmail.com>
2017-02-02 22:56:46 +0900

    impl check kyouen

--- lib/usecase/check_kyouen.rb
+++ lib/usecase/check_kyouen.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
+require 'matrix'
+
 module Usecase
   class CheckKyouen
+    KyouenData = Struct.new(:points, :lineKyouen)
+
     class << self
       def check(stage)
         stones = to_stones(stage)
@@ -16,10 +20,69 @@ module Usecase
         size = Math.sqrt(stage.length)
         size.to_i.times do |i|
           size.to_i.times do |j|
-            stones.push([j, i]) if stage[i * size + j] == '1'
+            stones.push(Vector[j, i]) if stage[i * size + j] == '1'
           end
         end
         stones
+      end
+
+      def kyouen?(stones)
+        # p1,p2の垂直二等分線を求める
+        l12 = get_midperpendicular(stones[0], stones[1])
+        # p2,p3の垂直二等分線を求める
+        l23 = get_midperpendicular(stones[1], stones[2])
+
+        # 交点を求める
+        intersection123 = get_intersection(l12, l23)
+        if intersection123.nil?
+          # p1,p2,p3が直線上に存在する場合
+          l34 = get_midperpendicular(stones[2], stones[3])
+          intersection234 = get_intersection(l23, l34)
+          if intersection234.nil?
+            # p2,p3,p4が直線状に存在する場合
+            return KyouenData.new(stones, true)
+          end
+        else
+          dist1 = get_distance(stones[0], intersection123)
+          dist2 = get_distance(stones[3], intersection123)
+          if (dist1 - dist2).abs < 0.0000001
+            return KyouenData.new(stones, false)
+          end
+        end
+        nil
+      end
+
+      def get_distance(p1, p2)
+        dist = p1 - p2
+        dist.norm
+      end
+
+      def get_intersection(l1, l2)
+        f1 = l1[1, 0] - l1[0, 0]
+        g1 = l1[1, 1] - l1[0, 1]
+        f2 = l2[1, 0] - l2[0, 0]
+        g2 = l2[1, 1] - l2[0, 1]
+
+        det = (f2 * g1 - f1 * g2).to_f
+        return nil if det.zero?
+
+        dx = l2[0, 0] - l1[0, 0]
+        dy = l2[0, 1] - l1[0, 1]
+        t1 = (f2 * dy - g2 * dx) / det
+
+        Vector[l1[0, 0] + f1 * t1, l1[0, 1] + g1 * t1]
+      end
+
+      def get_midperpendicular(p1, p2)
+        midpoint = get_midpoint(p1, p2)
+        diff = p1 - p2
+        gradient = Vector[diff[1], diff[0] * -1]
+
+        Matrix[midpoint.to_a, (midpoint + gradient).to_a]
+      end
+
+      def get_midpoint(p1, p2)
+        (p1 + p2) / 2.0
       end
     end
   end
--- spec/usecase/check_kyouen_spec.rb
+++ spec/usecase/check_kyouen_spec.rb
@@ -18,19 +18,77 @@ describe Usecase::CheckKyouen, lib: true do
     it 'check stone positions' do
       result = Usecase::CheckKyouen.send(:to_stones, '111100000000000000000000000000000000')
       expect(result.size).to eq 4
-      expect(result).to include([0, 0])
-      expect(result).to include([1, 0])
-      expect(result).to include([2, 0])
-      expect(result).to include([3, 0])
+      expect(result).to include(Vector[0, 0])
+      expect(result).to include(Vector[1, 0])
+      expect(result).to include(Vector[2, 0])
+      expect(result).to include(Vector[3, 0])
     end
 
     it 'check stone positions' do
       result = Usecase::CheckKyouen.send(:to_stones, '000000010000001000001100000000000000')
       expect(result.size).to eq 4
-      expect(result).to include([1, 1])
-      expect(result).to include([2, 2])
-      expect(result).to include([2, 3])
-      expect(result).to include([3, 3])
+      expect(result).to include(Vector[1, 1])
+      expect(result).to include(Vector[2, 2])
+      expect(result).to include(Vector[2, 3])
+      expect(result).to include(Vector[3, 3])
+    end
+  end
+
+  describe :is_kyouen do
+    it 'is not kyouen' do
+      stones = Usecase::CheckKyouen.send(:to_stones, '000000000000100100001100000000000000')
+      result = Usecase::CheckKyouen.send(:kyouen?, stones)
+      expect(result).to be_nil
+    end
+
+    it 'is kyouen' do
+      stones = Usecase::CheckKyouen.send(:to_stones, '000000000000001100001100000000000000')
+      result = Usecase::CheckKyouen.send(:kyouen?, stones)
+      expect(result).to eq Usecase::CheckKyouen::KyouenData.new(stones, false)
+    end
+
+    it 'is kyouen' do
+      stones = Usecase::CheckKyouen.send(:to_stones, '000000000000000000010010001100000000')
+      result = Usecase::CheckKyouen.send(:kyouen?, stones)
+      expect(result).to eq Usecase::CheckKyouen::KyouenData.new(stones, false)
+    end
+
+    it 'is kyouen' do
+      stones = Usecase::CheckKyouen.send(:to_stones, '000000000000101011000000000000000000')
+      result = Usecase::CheckKyouen.send(:kyouen?, stones)
+      expect(result).to eq Usecase::CheckKyouen::KyouenData.new(stones, true)
+    end
+  end
+
+  describe :get_intersection do
+    it 'get intersection' do
+      l1 = Matrix[[1, 1], [3, 3]]
+      l2 = Matrix[[0, 4], [4, 0]]
+      result = Usecase::CheckKyouen.send(:get_intersection, l1, l2)
+      expect(result).to eq Vector[2, 2]
+    end
+
+    it 'get intersection 2' do
+      l1 = Matrix[[1, 1], [3, 3]]
+      l2 = Matrix[[0, 3], [10, 3]]
+      result = Usecase::CheckKyouen.send(:get_intersection, l1, l2)
+      expect(result).to eq Vector[3, 3]
+    end
+  end
+
+  # 垂直二等分線
+  describe :get_midperpendicular do
+    it 'get midperpendicular' do
+      result = Usecase::CheckKyouen.send(:get_midperpendicular, Vector[1, 3], Vector[10, 20])
+      expect(result).to eq Matrix[[5.5, 11.5], [-11.5, 20.5]]
+    end
+  end
+
+  # 中点
+  describe :get_midpoint do
+    it 'get midpoint' do
+      result = Usecase::CheckKyouen.send(:get_midpoint, Matrix[[1, 3]], Matrix[[10, 20]])
+      expect(result).to eq Matrix[[5.5, 11.5]]
     end
   end
 end
